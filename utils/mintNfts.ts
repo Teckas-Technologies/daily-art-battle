1e12ce3b3363b615dfc2d24a1ec758ea33947616
sathish <sathish@kalakendradao.org>
2024-08-12 18:25:38 +0530

    Updated minting nft

--- utils/mintNfts.ts
+++ utils/mintNfts.ts
@@ -6,6 +6,7 @@ import { participationMint,connectAccount } from "./participationMint";
 import spinner from "./spinnerUtils";
 import uploadArweave from "./uploadArweave";
 import {  execute,  transfer,TransferArgs  } from "@mintbase-js/sdk"
+import { ART_BATTLE_CONTRACT } from "@/config/constants";
 
 interface Transfer {
   receiverId: string;
@@ -18,40 +19,33 @@ export const mintNfts = async (): Promise<void> => {
     const battle = await Battle.findOne({
         isNftMinted: false,
         isBattleEnded: true
-    });
-    const ress = await spinner();
-    console.log("Uploading arweave")
-    const response = await uploadArweave(ress);
-    battle.grayScale = response.url;
-    battle.grayScaleReference = response.referenceUrl;
+    }); 
+    // const ress = await spinner();
+    // console.log("Uploading arweave")
+    // const response = await uploadArweave(ress);
+    // battle.grayScale = response.url;
+    // battle.grayScaleReference = response.referenceUrl;
     console.log("Fetching completed battles",battle);
     if(battle){
-      const votes = battle.artAVotes+battle.artBVotes;
-      if(votes>0){
-      const tokenIds =  await mintNFTsForParticipants(votes,battle.grayScale,battle.grayScaleReference);
-      const voters = await mergeVoters(battle.artAvoters,battle.artBvoters);
-      await handleTransfer(tokenIds,voters);
+      if(battle.artAVotes>0){
+      const tokenIds =  await mintNFTsForParticipants(battle.artAVotes,battle.artAcolouredArt,battle.artAcolouredArtReference);
+      await handleTransfer(tokenIds,battle.artAvoters);
       }
-  
+      if(battle.artBVotes>0){
+        const tokenIds =  await mintNFTsForParticipants(battle.artBVotes,battle.artBcolouredArt,battle.artBcolouredArtReference);
+        await handleTransfer(tokenIds,battle.artBvoters);
+      }
       if(battle.isSpecialWinnerMinted==false){
-        const artAspecialWinner = selectRandomWinner(battle.artAvoters);
-        const artBspecialWinner = selectRandomWinner(battle.artBvoters);
+        const voters = mergeVoters(battle.artAvoters,battle.artBvoters);
+        const specialWinner = selectRandomWinner(voters);
         let tokenIdA;
-        let tokenIdB;
-        console.log("art A",artAspecialWinner);
-        console.log("art B",artBspecialWinner);
-        if (artAspecialWinner || artBspecialWinner) {
+        console.log("special winner",specialWinner);
+        if (specialWinner) {
            console.log("Winner")
            let logs1;
-           let logs2;
-           if(artAspecialWinner){
-              const res1 =  await serverMint(artAspecialWinner, battle.artAcolouredArt, battle.artAcolouredArtReference,true);
+              const res1 =  await serverMint(specialWinner, battle.grayScale, battle.grayScaleReference,true);
               logs1 =  res1.receipts_outcome.map((outcome :any)=> outcome.outcome.logs).flat();
-            }
-           if(artBspecialWinner){
-             const  res2 =  await serverMint(artBspecialWinner, battle.artBcolouredArt, battle.artBcolouredArtReference,true);
-             logs2 = res2.receipts_outcome.map((outcome :any)=> outcome.outcome.logs).flat();
-            }
+         
            if(logs1){
            const tokenIds1 = logs1.map((log:any) => {
             const match = log.match(/EVENT_JSON:(.*)/);
@@ -67,51 +61,32 @@ export const mintNfts = async (): Promise<void> => {
           console.log('Token IDs:', tokenIds1);
           tokenIdA = tokenIds1[0];
         }
-          if(logs2){
-           const tokenIds2 = logs2.map((log:any) => {
-            const match = log.match(/EVENT_JSON:(.*)/);
-            if (match && match[1]) {
-              const eventData = JSON.parse(match[1]);
-              if (eventData.data && eventData.data.length > 0) {
-                return eventData.data[0].token_ids;
-              }
-            }
-            return null;
-          }).filter((tokenIds:any) => tokenIds !== null).flat();
-          
-          console.log('Token IDs:', tokenIds2[0]);
-          tokenIdB = tokenIds2[0];
-        }
       }
-        battle.artAspecialWinner = artAspecialWinner;
-        battle.artBspecialWinner = artBspecialWinner;
-
-        await ArtTable.findOneAndUpdate(
-            { _id: battle.artAId }, 
-            { $set: { isCompleted: true,
-                specialWinner:artAspecialWinner  ,
-                tokenId : tokenIdA
-             } }, 
-            { new: true } 
-          );
-    
-          await ArtTable.findOneAndUpdate(
-            { _id: battle.artBId }, 
-            { $set: { isCompleted: true,
-                specialWinner:artBspecialWinner,
-                tokenId : tokenIdB
-               } }, 
-            { new: true } 
-          );
+        battle.specialWinner = specialWinner;
         battle.isNftMinted = true;
         battle.isSpecialWinnerMinted = true;
+        battle.tokenId = tokenIdA;
        const res =  await battle.save();
        console.log("saved",res);
         }
     }
 }
 
-const mintNFTsForParticipants = async (artVoters:number, grayScale:string,grayScaleReference:string ) => {
+const mintNFTsForParticipants = async (artVoters: number, grayScale: string, grayScaleReference: string): Promise<string[]> => {
+  const maxBatchSize = 99;
+  const tokenIds: string[] = [];
+
+  for (let i = 0; i < artVoters; i += maxBatchSize) {
+      const batchSize = Math.min(maxBatchSize, artVoters - i);
+      const batchTokenIds = await mintBatch(batchSize, grayScale, grayScaleReference);
+      tokenIds.push(...batchTokenIds);
+  }
+
+  return tokenIds;
+};
+
+
+const mintBatch = async (artVoters:number, grayScale:string,grayScaleReference:string ) => {
         const  res =   await participationMint(artVoters, grayScale, grayScaleReference, false);
        let logs = res.receipts_outcome.map((outcome :any)=> outcome.outcome.logs).flat();
        const tokenIds = logs.map((log:any) => {
@@ -129,30 +104,37 @@ const mintNFTsForParticipants = async (artVoters:number, grayScale:string,graySc
 };
 
 
-const handleTransfer = async (tokenIds:string[],voters:string[]): Promise<void> => {
+const handleTransfer = async (tokenIds: string[], voters: string[]): Promise<void> => {
   if (tokenIds.length !== voters.length) {
-    throw new Error('Arrays must be of the same length');
+      throw new Error('Arrays must be of the same length');
   }
 
-    let tokenList: Transfer[] = [];
+  const maxBatchSize = 99;
 
-    for (let i = 0; i < tokenIds.length; i++) {
-        let trans: Transfer = { receiverId: voters[i], tokenId: tokenIds[i] };
-        tokenList.push(trans);
-    }
+  for (let i = 0; i < tokenIds.length; i += maxBatchSize) {
+      const tokenChunk = tokenIds.slice(i, i + maxBatchSize);
+      const voterChunk = voters.slice(i, i + maxBatchSize);
 
-       console.log("tranfer")
+      let tokenList: Transfer[] = [];
+      for (let j = 0; j < tokenChunk.length; j++) {
+          let trans: Transfer = { receiverId: voterChunk[j], tokenId: tokenChunk[j] };
+          tokenList.push(trans);
+      }
+
+      console.log(`Transfer batch ${i / maxBatchSize + 1}`);
+
       const transferArgs: TransferArgs = {
-      contractAddress: "artbattle.mintspace2.testnet",
-      transfers: tokenList,
-    }
-    const account = await connectAccount();
-  await execute(
-    { account:account},
-    transfer(transferArgs),
-  );
-};
+          contractAddress: ART_BATTLE_CONTRACT,
+          transfers: tokenList,
+      };
 
+      const account = await connectAccount();
+      await execute(
+          { account: account },
+          transfer(transferArgs),
+      );
+  }
+};
 
 const selectRandomWinner = (votes: any[]): any => {
     if (votes.length === 0) return null;
@@ -160,14 +142,14 @@ const selectRandomWinner = (votes: any[]): any => {
     return votes[randomIndex];
 };
 
-      const mergeVoters = (votersA: any[],votersB:any[]): any => {
-          const voters:any[] = [];
-          for(const voter of votersA){
-            voters.push(voter);
-          }
-          for(const voter of votersB){
-            voters.push(voter);
-          }
+const mergeVoters = (votersA: any[],votersB:any[]): any => {
+    const voters:any[] = [];
+    for(const voter of votersA){
+      voters.push(voter);
+    }
+    for(const voter of votersB){
+      voters.push(voter);
+    }
 
-          return voters
-      };
+    return voters
+};
