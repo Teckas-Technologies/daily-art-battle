15fbfe34b94b92cbecccce80b99cab5aa9dee43d
sathish <sathish@kalakendradao.org>
2024-08-13 12:28:57 +0530

    Updated date and votes column

--- src/components/PreviousBattlesTable.tsx
+++ src/components/PreviousBattlesTable.tsx
@@ -41,9 +41,13 @@ const PreviousArtTable: React.FC<{ toggleUploadModal: () => void }> = ({
     }
   };
 
-  const formatDate = (timestamp: Date) => {
+  const formatDate = (timestamp: Date): string => {
     const date = new Date(timestamp);
-    return date.toLocaleDateString();
+    return date.toLocaleDateString('en-US', {
+      month: 'short',  
+      day: '2-digit',  
+      year: 'numeric'  
+    });
   };
   const handleSort = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const sortType = event.target.value
@@ -90,36 +94,6 @@ const PreviousArtTable: React.FC<{ toggleUploadModal: () => void }> = ({
                     RARE OWNER
                   </th>
                   <th
-                    className="text-white text-center cursor-pointer font-normal hover:underline"
-                  >
-                      <span className="flex items-center justify-center hover:underline">
-                        {/* <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                        width="1.25em"
-                        height="1em"
-                        viewBox="0 0 640 512"
-                        className="ml-1"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M608 320h-64v64h22.4c5.3 0 9.6 3.6 9.6 8v16c0 4.4-4.3 8-9.6 8H73.6c-5.3 0-9.6-3.6-9.6-8v-16c0-4.4 4.3-8 9.6-8H96v-64H32c-17.7 0-32 14.3-32 32v96c0 17.7 14.3 32 32 32h576c17.7 0 32-14.3 32-32v-96c0-17.7-14.3-32-32-32m-96 64V64.3c0-17.9-14.5-32.3-32.3-32.3H160.4C142.5 32 128 46.5 128 64.3V384zM211.2 202l25.5-25.3c4.2-4.2 11-4.2 15.2.1l41.3 41.6l95.2-94.4c4.2-4.2 11-4.2 15.2.1l25.3 25.5c4.2 4.2 4.2 11-.1 15.2L300.5 292c-4.2 4.2-11 4.2-15.2-.1l-74.1-74.7c-4.3-4.2-4.2-11 0-15.2"
-                        />
-                      </svg>
-                      VOTES */}
-                      <select
-                      onChange={handleSort}
-                            className="bg-white text-black   border border-gray-600 rounded-lg cursor-pointer"
-                          >
-
-                            <option value="voteDsc">Vote DSC</option>
-                            <option value="voteAsc">Vote ASC</option>
-                            
-                          </select>
-                      
-                    </span>
-                 
-                  </th>
-                  <th
                   
                     className="text-white text-center cursor-pointer font-normal hover:underline"
                   >
@@ -147,10 +121,12 @@ const PreviousArtTable: React.FC<{ toggleUploadModal: () => void }> = ({
                       DATE */}
                            <select
                                  onChange={handleSort}
-                            className="bg-white text-black border border-gray-600 rounded-lg cursor-pointer"
+                            className="bg-white lg:ml-10 md:ml-10 text-black border border-gray-600 rounded-lg cursor-pointer"
                           >
-                            <option value="dateDsc">Date DSC</option>
-                            <option value="dateAsc">Date ASC</option>
+                              <option value="dateDsc">Date DSC</option>
+                              <option value="dateAsc">Date ASC</option>
+                               <option value="voteDsc">Vote DSC</option>
+                               <option value="voteAsc">Vote ASC</option>
                           </select>
                     </span>
                   </th>
@@ -265,9 +241,6 @@ const PreviousArtTable: React.FC<{ toggleUploadModal: () => void }> = ({
                           )}
                         <br />
                       </td>
-                      <td className="px-4 py-2 text-xs sm:text-2xl font-medium break-words  break-all text-black text-center special-winner">
-                    {Number(battle.artAVotes) + Number(battle.artBVotes)}
-                      </td>
                       <td
                         className="px-4 py-2 text-xs  sm:text-2xl font-medium break-words  break-all text-black text-center special-winner"
                         style={{
@@ -278,6 +251,8 @@ const PreviousArtTable: React.FC<{ toggleUploadModal: () => void }> = ({
                         {battle.startTime
                           ? formatDate(battle.startTime)
                           : "Date not available"}
+                          <br></br>
+                             {Number(battle.artAVotes) + Number(battle.artBVotes)} votes
                       </td>
                     </tr>
                   </>
