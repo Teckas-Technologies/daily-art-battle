af451e4e47f250212d31573c976994120abf2929
Sathish M <153342012+SathishTeckas@users.noreply.github.com>
2024-08-21 16:46:10 +0530

    Update spinner.ts

--- pages/api/spinner.ts
+++ pages/api/spinner.ts
@@ -4,7 +4,7 @@ import spinner from '../../utils/spinnerUtils';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try{
-     const re = await spinner();
+   //  const re = await spinner();
     res.status(200).json(re);
     }catch(error){
         console.log("error");
