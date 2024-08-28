6905753fdcced6438301a20575577fb9d895f707
sathish <sathish@kalakendradao.org>
2024-08-28 20:46:43 +0530

    Updated

--- src/app/admin/components/CampaignForm.tsx
+++ src/app/admin/components/CampaignForm.tsx
@@ -36,6 +36,8 @@ const CampaignForm: React.FC<CampaignFormProps> = ({
     uploadMediaToAzure,
   } = useFetchCampaignByTitle();
   const [loading, setLoading] = useState(false);
+  const [loadingVideo, setLoadingVideo] = useState(false);
+  const [loadingLogo, setLoadingLogo] = useState(false);
 
   useEffect(() => {
     if (campaign) {
@@ -58,12 +60,22 @@ const CampaignForm: React.FC<CampaignFormProps> = ({
     if (files && (id === "video" || id === "logo")) {
       const file = files[0];
       setDisabled(true);
+      if (id === "video") {
+        setLoadingVideo(true);
+      } else if (id === "logo") {
+        setLoadingLogo(true);
+      }
       const url = await uploadMediaToAzure(file);
       console.log(url)
       setFormData((prevData) => ({
         ...prevData,
         [id]: url,
       }));
+      if (id === "video") {
+        setLoadingVideo(false);
+      } else if (id === "logo") {
+        setLoadingLogo(false);
+      }
       setDisabled(false);
     } else {
       setFormData((prevData) => ({
@@ -219,6 +231,27 @@ const CampaignForm: React.FC<CampaignFormProps> = ({
                 onChange={handleChange}
                 className="block w-full text-black py-2 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500"
               />
+                {loadingVideo && (
+                  <div role="status" className="ml-2">
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08167 50.5908C9.08167 73.1827 27.4081 91.5092 50 91.5092C72.5919 91.5092 90.9183 73.1827 90.9183 50.5908C90.9183 27.9988 72.5919 9.67238 50 9.67238C27.4081 9.67238 9.08167 27.9988 9.08167 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5531C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7236 75.2124 7.41289C69.5422 4.10219 63.2754 1.94025 56.7017 1.05126C51.7667 0.3679 46.7826 0.446057 41.888 1.27846C39.4174 1.69323 37.9924 4.19778 38.6295 6.62326C39.2667 9.04873 41.7569 10.4717 44.2295 10.1071C48.4302 9.459 52.7191 9.45903 56.9198 10.1071C61.7694 10.8623 66.4018 12.7185 70.4864 15.5528C74.5711 18.3872 78.0398 22.1336 80.654 26.582C82.8732 30.082 84.4804 33.8754 85.4169 37.818C86.0605 40.1759 88.5423 41.678 91.0011 41.0399L93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                )}
             </div>
             <div className="mb-5">
               <label
@@ -234,6 +267,27 @@ const CampaignForm: React.FC<CampaignFormProps> = ({
                 onChange={handleChange}
                 className="block w-full text-black py-2 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500"
               />
+                {loadingLogo && (
+                  <div role="status" className="mt-2 ml-2">
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08167 50.5908C9.08167 73.1827 27.4081 91.5092 50 91.5092C72.5919 91.5092 90.9183 73.1827 90.9183 50.5908C90.9183 27.9988 72.5919 9.67238 50 9.67238C27.4081 9.67238 9.08167 27.9988 9.08167 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5531C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7236 75.2124 7.41289C69.5422 4.10219 63.2754 1.94025 56.7017 1.05126C51.7667 0.3679 46.7826 0.446057 41.888 1.27846C39.4174 1.69323 37.9924 4.19778 38.6295 6.62326C39.2667 9.04873 41.7569 10.4717 44.2295 10.1071C48.4302 9.459 52.7191 9.45903 56.9198 10.1071C61.7694 10.8623 66.4018 12.7185 70.4864 15.5528C74.5711 18.3872 78.0398 22.1336 80.654 26.582C82.8732 30.082 84.4804 33.8754 85.4169 37.818C86.0605 40.1759 88.5423 41.678 91.0011 41.0399L93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                )}
             </div>
             <div className="mb-5">
               <label
