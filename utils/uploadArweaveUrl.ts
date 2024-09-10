75fb20782519dee661a957b38a007d8811f8ea31
Johnson <johnson@kalakendradao.org>
2024-09-10 16:20:05 +0530

    used upload reference

--- utils/uploadArweaveUrl.ts
+++ utils/uploadArweaveUrl.ts
@@ -1,13 +1,25 @@
-import { uploadBuffer } from "@mintbase-js/storage";
+import { uploadBuffer, uploadReference } from "@mintbase-js/storage";
 export default async function uploadArweaveUrl(url: string) {
-    const metadata = {
-      title: "Art Battle",
-      media: url,
-    };
-
-    const referenceBuffer = Buffer.from(JSON.stringify(metadata));
-    const referenceResult = await uploadBuffer(referenceBuffer,"application/json","File");
-    const referenceUrl = `https://arweave.net/${referenceResult.id}`;
-    console.log(url,referenceUrl);
-    return {url,referenceUrl};
+  const metadata = {
+    title: "Art Battle",
+    media: url
+  }
+  const referenceResult = await uploadReference(metadata);
+  const referenceUrl = `https://arweave.net/${referenceResult.id}`;
+  console.log(url, referenceUrl);
+  return { url, referenceUrl };
 }
+
+// import { uploadBuffer } from "@mintbase-js/storage";
+// export default async function uploadArweaveUrl(url: string) {
+//     const metadata = {
+//       title: "Art Battle",
+//       media: url,
+//     };
+
+//     const referenceBuffer = Buffer.from(JSON.stringify(metadata));
+//     const referenceResult = await uploadBuffer(referenceBuffer,"application/json","File");
+//     const referenceUrl = `https://arweave.net/${referenceResult.id}`;
+//     console.log(url,referenceUrl);
+//     return {url,referenceUrl};
+// }
