e9fe9f8b6d135f12461f32cca88a588615c6c797
Johnson <johnson@kalakendradao.org>
2024-09-10 16:54:55 +0530

    commented code removed

--- utils/uploadArweaveUrl.ts
+++ utils/uploadArweaveUrl.ts
@@ -1,4 +1,4 @@
-import { uploadBuffer, uploadReference } from "@mintbase-js/storage";
+import { uploadReference } from "@mintbase-js/storage";
 export default async function uploadArweaveUrl(url: string) {
   const metadata = {
     title: "Art Battle",
@@ -9,17 +9,3 @@ export default async function uploadArweaveUrl(url: string) {
   console.log(url, referenceUrl);
   return { url, referenceUrl };
 }
-
-// import { uploadBuffer } from "@mintbase-js/storage";
-// export default async function uploadArweaveUrl(url: string) {
-//     const metadata = {
-//       title: "Art Battle",
-//       media: url,
-//     };
-
-//     const referenceBuffer = Buffer.from(JSON.stringify(metadata));
-//     const referenceResult = await uploadBuffer(referenceBuffer,"application/json","File");
-//     const referenceUrl = `https://arweave.net/${referenceResult.id}`;
-//     console.log(url,referenceUrl);
-//     return {url,referenceUrl};
-// }
