96a707ae5656c6d230ec23392ab91afef35746a3
Johnson <johnson@kalakendradao.org>
2024-09-10 10:51:14 +0530

    added File type in upload arweave

--- utils/uploadArweaveUrl.ts
+++ utils/uploadArweaveUrl.ts
@@ -7,7 +7,7 @@ export default async function uploadArweaveUrl(url: string) {
 
     const referenceBuffer = Buffer.from(JSON.stringify(metadata));
     console.log("referenceBuffer >> ", referenceBuffer)
-    const referenceResult = await uploadBuffer(referenceBuffer,"application/json","JSON");
+    const referenceResult = await uploadBuffer(referenceBuffer,"application/json","File");
     const referenceUrl = `https://arweave.net/${referenceResult.id}`;
     console.log(url,referenceUrl);
     return {url,referenceUrl};
