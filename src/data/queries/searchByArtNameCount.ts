eabf04c523c4a98eb54df696b5ba92a1c8d463f3
sathish <sathish@kalakendradao.org>
2024-11-14 12:08:33 +0530

    Updated

---
+++ src/data/queries/searchByArtNameCount.ts
@@ -0,0 +1,17 @@
+import { gql } from 'graphql-request';
+
+export const SEARCH_TOTAL = gql`
+  query MyQuery($nft_contract_ids: [String!], $owner: String!, $title: String!) {
+    mb_views_nft_tokens_aggregate(
+      where: {
+        nft_contract_id: { _in: $nft_contract_ids }
+        owner: { _eq: $owner }
+        title: { _ilike: $title }
+      }
+    ) {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
