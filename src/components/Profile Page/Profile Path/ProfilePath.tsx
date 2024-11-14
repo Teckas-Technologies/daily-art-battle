2aea7b218a404a8e48d95219725fa3052fe6d6a2
SharmilaBlessyS <sharmila@kalakendradao.org>
2024-11-14 17:12:18 +0530

    dailycheckin added

--- src/components/Profile Page/Profile Path/ProfilePath.tsx
+++ src/components/Profile Page/Profile Path/ProfilePath.tsx
@@ -3,13 +3,13 @@ import InlineSVG from 'react-inlinesvg'
 
 const ProfilePath = () => {
   return (
-    <div className="flex gap-0 md:gap-3 items-center mt-[100px] md:mt-[140px] mb-[16px] md:mb-[20px]">
-      <button className="text-sm md:text-base">GFXvs</button>
+    <div className="flex gap-0 md:gap-1 items-center mt-[120px] md:mt-[100px] md:mt-[140px] mb-[16px] md:mb-[20px]">
+      <button className="camapign-path-button text-sm md:text-base">GFXvs</button>
       <InlineSVG
         src="/icons/green-arrow.svg"
-        className="fill-current text-green-500 w-4 h-4 md:w-6 md:h-6"
+        className="w-4 h-4 md:w-5 md:h-5"
       />
-      <h3 className="text-[#00FF00] spartan-medium underline cursor-pointer text-xs md:text-lg">
+      <h3 className="text-[#00FF00] spartan-medium underline cursor-pointer text-xs md:text-sm">
         My Profile
       </h3>
     </div>
