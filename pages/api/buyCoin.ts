35a991b90cda6f4b9f376cb0868dec00b91f3d9e
sathish <sathish@kalakendradao.org>
2024-11-14 20:45:58 +0530

    Updated design and added api for buy coin

--- pages/api/buyCoin.ts
+++ pages/api/buyCoin.ts
@@ -41,7 +41,7 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse) {
                           }
                           const totalNear = coins / oneNearInGfxCoin;
 
-                          return res.status(200).json({ totalNear });                          
+                          return res.status(200).json({ totalNear });                           
                     }else if(queryFilter=="usdc"){
                         const oneUsdcInGfxCoin: number = GFXCOIN_PER_USDC;
                         const coins = Number(req.query.coins);
