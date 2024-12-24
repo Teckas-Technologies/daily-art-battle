2e974c153272875aeec473b470c3779a54781eec
SathishTeckas <sathish@teckastechnologies.com>
2024-12-24 11:30:20 +0530

    updated issues

--- pages/api/raffleTicket.ts
+++ pages/api/raffleTicket.ts
@@ -60,7 +60,7 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
             await newTransaction.save();
             const trans= await createTransaction(requiredCoins, AdminTransactionType.RECEIVED_FROM_RAFFLE,email);
             const updatedBalance = await updateAdminBalance(requiredCoins, AdminTransactionType.EARN);
-            await artistReward(email,ticketCount);
+            await artistReward(artId,ticketCount);
             res.status(201).json({ message: 'Raffle tickets purchased successfully' });
         }catch(error:any){
             res.status(400).json({error:error.message});
